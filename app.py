7d97e033e9afba6d48d037d5b3766f73a36afbfb
umapan <umapanpoosuwan@gmail.com>
2017-04-13 14:22:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,13 +36,13 @@ def webhook():
 
 
 def processRequest(req):
-    if req.get("result").get("action") != "askstock":
+    if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://google-stocks.herokuapp.com/?code"
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + urlencode({'BKK:': yql_query}) + "&format=json"
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
@@ -52,22 +52,41 @@ def processRequest(req):
 def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("stock_name")
+    city = parameters.get("geo-city")
     if city is None:
         return None
 
-    return city
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 
 def makeWebhookResult(data):
-
-    symbol = data[0].get('t')
-    price = data[0].get('l')
-    if (symbol is None) or (price is None):
+    query = data.get('query')
+    if query is None:
         return {}
 
-    speech = "Stock :" + symbol + " Price: " + price
-    speech = "Hello"
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
     print("Response:")
     print(speech)
 
