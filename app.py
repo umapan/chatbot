a1c99cce395da8a31c2b377d9c43e7a55c7006b2
umapan <umapanpoosuwan@gmail.com>
2017-04-13 12:35:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,32 +60,13 @@ def makeYqlQuery(req):
 
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
+
+    symbol = data[0].get('t')
+    price = data[0].get('l')
+    if (symbol is None) or (price is None):
         return {}
 
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Stock :" + symbol + " Price: " + price
 
     print("Response:")
     print(speech)
