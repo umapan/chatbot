a69dfb0604a4c0007f37ad8b3092dda6fea1d329
umapan <umapanpoosuwan@gmail.com>
2017-04-13 12:37:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    baseurl = "https://google-stocks.herokuapp.com/?code=BKK:"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    yql_url = baseurl + yql_query + "&format=json"
     result = urlopen(yql_url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
@@ -56,36 +56,17 @@ def makeYqlQuery(req):
     if city is None:
         return None
 
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+    return city
 
 
 def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
+
+    symbol = data[0].get('t')
+    price = data[0].get('l')
+    if (symbol is None) or (price is None):
         return {}
 
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+    speech = "Stock :" + symbol + " Price: " + price
 
     print("Response:")
     print(speech)
